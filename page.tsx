c3e8b6e5951ad5f4653209ccd9724c6500fa562d
Vanth <vanth@example.com>
2025-01-05 19:28:03 +0700

    5/1/2025

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import { auth } from "@/auth";
+import HomePage from "@/components/layout/homepage";
+
+export default async function Home() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
